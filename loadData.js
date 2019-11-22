ef0c96b8562db83faa913455e324da5445a737da
Fourth of August <fourthofaugust@users.noreply.github.com>
2019-11-22 11:50:32 -0500

    Create loadData.js

---
+++ loadData.js
@@ -0,0 +1,44 @@
+const {google} = require("googleapis");
+const {auth} = require("google-auth-library");
+var sqladmin = google.sqladmin("v1beta4");
+
+exports.loadData = (event, context) => {
+//TODO Can I use async at an event function level?
+    async function triggerDataLoad(gcsEvent) {
+        try {
+            const authRes = await auth.getApplicationDefault();
+            const authClient = authRes.credential;
+
+            const request = {
+                project: "your-project-name",
+                instance: "your-sql-instance-name",
+                resource: {
+                    "importContext": {
+                        "kind": "sql#importContext",
+                        "fileType": "SQL",
+                        "uri": `gs://${gcsEvent.bucket}/${gcsEvent.name}`,
+                        "database": "your-db-name"
+                    }
+                },
+                auth: authClient,
+            };
+
+            sqladmin.instances.import(request, function (err, response) {
+                if (err) {
+                    console.error('Error connecting to Cloud SQL');
+                    console.error(err);
+                    return;
+                }
+                console.log('Import Successful...');
+                console.log(response.data);
+            });
+
+        } catch (err) {
+            console.error('Error retrieving default auth credentials...);
+            console.error(err);
+        }
+    }
+
+    triggerDataLoad(event);
+
+};
